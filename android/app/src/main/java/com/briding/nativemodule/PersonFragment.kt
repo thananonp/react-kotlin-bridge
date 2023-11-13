edb1560d0c883d7a0cb0f2e7873fd87084c2d544
Thananon Pongsuwan <thenightraven7407@gmail.com>
2023-11-13 15:39:14 +0700

    feat: add lifecycle test

--- android/app/src/main/java/com/briding/nativemodule/PersonFragment.kt
+++ android/app/src/main/java/com/briding/nativemodule/PersonFragment.kt
@@ -1,6 +1,7 @@
 package com.briding.nativemodule
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,18 +18,29 @@ import androidx.compose.material.Text
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 
 @OptIn(ExperimentalMaterialApi::class)
 class PersonFragment(
     private var viewModel: PersonBlueprint
 ) : Fragment() {
+    val TAG = "PersonFragment"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
@@ -41,6 +53,34 @@ class PersonFragment(
                 val pullRefreshState = rememberPullRefreshState(
                     refreshing = isRefreshing, onRefresh = viewModel.refresh
                 )
+                
+                val lifecycle = rememberLifecycleEvent()
+                
+                LaunchedEffect(key1 = lifecycle){
+                    when(lifecycle){
+                        Lifecycle.Event.ON_CREATE -> {
+                            Log.d(TAG, "onCreate")
+                        }
+                        Lifecycle.Event.ON_START -> {
+                            Log.d(TAG, "onStart")
+                        }
+                        Lifecycle.Event.ON_RESUME -> {
+                            Log.d(TAG, "onResume")
+                        }
+                        Lifecycle.Event.ON_PAUSE -> {
+                            Log.d(TAG, "onPause")
+                        }
+                        Lifecycle.Event.ON_STOP -> {
+                            Log.d(TAG, "onStop")
+                        }
+                        Lifecycle.Event.ON_DESTROY -> {
+                            Log.d(TAG, "onDestroy")
+                        }
+                        Lifecycle.Event.ON_ANY -> {
+                            Log.d(TAG, "onAny")
+                        }
+                    }
+                }
 
                 MaterialTheme {
                     Box(
@@ -76,4 +116,19 @@ class PersonFragment(
         }
 
     }
+}
+
+@Composable
+fun rememberLifecycleEvent(lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current): Lifecycle.Event {
+    var state by remember { mutableStateOf(Lifecycle.Event.ON_ANY) }
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            state = event
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+    return state
 }
